1419e8fbd1955b5fddcfb0e977c08bfe1cdafb68
Raj Shah <rajshah.co.in@gmail.com>
2020-07-25 18:34:22 +0530

    ReactJs Application

---
+++ public/precache-manifest.e063a9b29c96320223cb9aac8f46e719.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68ef0068b98a7f0cd922cb1e08591c73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c04b11d19855fa068d79",
+    "url": "/static/css/2.72a609cf.chunk.css"
+  },
+  {
+    "revision": "09b511e813f908b4a2b1",
+    "url": "/static/css/main.438fdd75.chunk.css"
+  },
+  {
+    "revision": "c04b11d19855fa068d79",
+    "url": "/static/js/2.00d1b3d6.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.00d1b3d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09b511e813f908b4a2b1",
+    "url": "/static/js/main.a8062d3a.chunk.js"
+  },
+  {
+    "revision": "aff20233fc33123ab008",
+    "url": "/static/js/runtime-main.74f98a6f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
